14eaf1febd22b1b1ec641e93995318a0413d8cdc
Rafael Montes <rafaelmontes1997@gmail.com>
2021-05-12 15:10:40 -0400

    Test db

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://rafael_m:oakland2021@mern-stack-shard-00-00.acx9h.mongodb.net:27017,mern-stack-shard-00-01.acx9h.mongodb.net:27017,mern-stack-shard-00-02.acx9h.mongodb.net:27017/dota?ssl=true&replicaSet=atlas-k7kxyz-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log("mongoDB Connected..."))
   .catch(err => console.log("error"))
